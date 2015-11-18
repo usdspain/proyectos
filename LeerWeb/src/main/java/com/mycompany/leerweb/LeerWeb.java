8f17cc6176ad96c957a56ff9b38cf370f9d177a6
Vicent Picornell <usdspain@gmail.com>
2015-11-18 13:31:41 +0100

    Obtenemos la web http:\\:www.yahoo.com

--- LeerWeb/src/main/java/com/mycompany/leerweb/LeerWeb.java
+++ LeerWeb/src/main/java/com/mycompany/leerweb/LeerWeb.java
@@ -5,12 +5,38 @@
  */
 package com.mycompany.leerweb;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+
 /**
  *
  * @author formacion
  */
 public class LeerWeb {
-    public static void main(String [] args){
+    public static void main(String [] args) throws MalformedURLException, IOException{
         
+      //  String web=conexionGET("http:\\www.terra.es", "http");
+            
+   
+        URL yahoo = new URL("http://www.yahoo.com/");
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
     }
+   
 }
